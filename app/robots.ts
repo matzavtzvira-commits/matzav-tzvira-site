41ead80c02a05ccda37d7a723650882f5bce850f
Rivky Weiss <matzavtzvira@gmail.com>
2026-05-05 15:55:39 +0300

    עדכון SITE_URL לדומיין הנכון

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://www.matzav-tzvira.co.il"; // עדכני לדומיין האמיתי
+const SITE_URL = "https://matzavtzvira.co.il";
 
 export default function robots(): MetadataRoute.Robots {
   return {
